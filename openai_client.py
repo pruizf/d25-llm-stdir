33708796f0373b9f6588ab4ac53c84fca1c853de
Pablo Ruiz Fabo <ruizfabo@unistra.fr>
2024-09-22 18:43:42 +0200

    open ai client skips call if response already available

--- openai_client.py
+++ openai_client.py
@@ -51,9 +51,12 @@ if __name__ == "__main__":
   parser.add_argument("corpus", help="Corpus to run the model on")
   parser.add_argument("model", help="Model to use for generating the response")
   parser.add_argument("prompt_mode", help="Prompting strategy to use")
+  parser.add_argument("--ignore-existing", "-i", action="store_true",
+                      help="Run even if results for same batch_name already exist (Used to continue an interrupted batch")
   args = parser.parse_args()
   assert args.model in cf.llm_list, f"Model {args.model} not in {cf.llm_list}"
-  assert args.batch_name not in os.listdir(cf.response_base_dir), f"Batch {args.batch_name} already exists"
+  if not args.ignore_existing:
+    assert args.batch_name not in os.listdir(cf.response_base_dir), f"Batch {args.batch_name} already exists"
   assert args.batch_name.startswith("batch_"), "Batch name must start with 'batch_'"
   assert args.prompt_mode in cf.prompting_modes, f"Prompting strategy {args.prompting} not in {cf.prompting_modes}"
 
@@ -78,6 +81,18 @@ if __name__ == "__main__":
   for idx, row in stdirs.iterrows():
     if False and idx > 3:
       break
+
+    # do not call API if the response is already available
+    out_comp_fn = os.path.join(cf.completions_dir.format(batch_id=args.batch_name),
+                               f"completion_{str.zfill(str(idx), 4)}_{args.model}.json")
+    out_resp_fn = os.path.join(cf.postpro_response_dir.format(batch_id=args.batch_name),
+                               f"postpro_response_{str.zfill(str(idx), 4)}_{args.model}.json")
+    out_prompt_fn = os.path.join(cf.prompts_dir.format(batch_id=args.batch_name),
+                                 f"prompt_{str.zfill(str(idx), 4)}_{args.model}.txt")
+    if os.path.exists(out_comp_fn) and os.path.exists(out_resp_fn) and os.path.exists(out_prompt_fn):
+      print(f"# Skipping stage direction: {idx}")
+      continue
+
     # general prompt
     if args.prompt_mode == "two-shot":
       prompt = pr.gen_prompt.format(
@@ -109,19 +124,18 @@ if __name__ == "__main__":
     print(f"- Response time: {td} ms")
     print()
 
-    out_comp_fn = os.path.join(cf.completions_dir.format(batch_id=args.batch_name),
-                               f"completion_{str.zfill(str(idx), 4)}_{args.model}.json")
+    # out_comp_fn = os.path.join(cf.completions_dir.format(batch_id=args.batch_name),
+    #                            f"completion_{str.zfill(str(idx), 4)}_{args.model}.json")
     with (open(out_comp_fn, "w") as out_comp_fh):
       jso = completion.model_dump_json()
       json.dump(jso, out_comp_fh, indent=2)
 
-    out_resp_fn = os.path.join(cf.postpro_response_dir.format(batch_id=args.batch_name),
-                               f"postpro_response_{str.zfill(str(idx), 4)}_{args.model}.json")
+    # out_resp_fn = os.path.join(cf.postpro_response_dir.format(batch_id=args.batch_name),
+    #                            f"postpro_response_{str.zfill(str(idx), 4)}_{args.model}.json")
     with (open(out_resp_fn, "w") as out_resp_fh):
       json.dump(jresp, out_resp_fh, indent=2)
 
-    breakpoint()
-    out_prompt_fn = os.path.join(cf.prompts_dir.format(batch_id=args.batch_name),
-                                 f"prompt_{str.zfill(str(idx), 4)}_{args.model}.txt")
+    # out_prompt_fn = os.path.join(cf.prompts_dir.format(batch_id=args.batch_name),
+    #                              f"prompt_{str.zfill(str(idx), 4)}_{args.model}.txt")
     with (open(out_prompt_fn, "w") as out_prompt_fh):
       out_prompt_fh.write(prompt)
