89d226ef904b01592436e080999b5f5120869131
Pablo Ruiz Fabo <ruizfabo@unistra.fr>
2024-09-22 18:44:08 +0200

    tidies up

--- openai_client.py
+++ openai_client.py
@@ -124,18 +124,12 @@ if __name__ == "__main__":
     print(f"- Response time: {td} ms")
     print()
 
-    # out_comp_fn = os.path.join(cf.completions_dir.format(batch_id=args.batch_name),
-    #                            f"completion_{str.zfill(str(idx), 4)}_{args.model}.json")
     with (open(out_comp_fn, "w") as out_comp_fh):
       jso = completion.model_dump_json()
       json.dump(jso, out_comp_fh, indent=2)
 
-    # out_resp_fn = os.path.join(cf.postpro_response_dir.format(batch_id=args.batch_name),
-    #                            f"postpro_response_{str.zfill(str(idx), 4)}_{args.model}.json")
     with (open(out_resp_fn, "w") as out_resp_fh):
       json.dump(jresp, out_resp_fh, indent=2)
 
-    # out_prompt_fn = os.path.join(cf.prompts_dir.format(batch_id=args.batch_name),
-    #                              f"prompt_{str.zfill(str(idx), 4)}_{args.model}.txt")
     with (open(out_prompt_fn, "w") as out_prompt_fh):
       out_prompt_fh.write(prompt)
