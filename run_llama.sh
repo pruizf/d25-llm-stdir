224d610ca2f8aeffc9350737b783f0d331e2f1ef
Pablo RUIZ FABO <ruizfabo@unistra.fr>
2024-09-28 11:30:45 +0200

    modif run llama script

--- run_llama.sh
+++ run_llama.sh
@@ -6,11 +6,14 @@
 
 #SBATCH -p publicgpu
 ##SBATCH -N 1
+##SBATCH -N 2-4
 #SBATCH -A lilpa
 ##SBATCH --gres=gpu:1
 ##SBATCH --constraint="gpup100|gpuv100|gpurtx5000|gpua100" # 16, 32, 16, 40 GB >16
-#SBATCH --constraint="gpup100|gpuv100|gpurtx5000|gpurtx6000|gpua100" # 16, 32, 16, 22.7, 40 GB >16 MORE CHOICES
-##SBATCH --constraint="gpuv100|gpurtx6000|gpua100" # 32, 22.7, 40 GB MOST DEMANDING
+##SBATCH --constraint="gpup100|gpuv100|gpurtx5000|gpurtx6000|gpua100" # 16, 32, 16, 22.7, 40 GB >16 MORE CHOICES
+#SBATCH --constraint="gpuv100|gpurtx6000|gpua100" # 32, 22.7, 40 GB MOST DEMANDING
+##SBATCH --constraint="gpuv100|gpua100" # 32, 22.7, 40 GB ONLY TOP MOST DEMANDING
+##SBATCH --constraint="gpurtx6000"
 
 # Usage: llama_client.py batch_name corpus_name model_name prompt_mode language
 #   - batch_name starts with 'batch_'
@@ -22,4 +25,4 @@
 # try to run python with -u (unbuffered) to see print messages on slurm log as they are issued
 # (otherwise they will be buffered and printed at the end of the job)
 
-time python3 -u llama_client.py $1 $2 $3 $4 $5
+time python3 -u llama_client.py $1 $2 $3 $4 $5 -i
